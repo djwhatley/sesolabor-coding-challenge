e90bcd11d0a74dccbd84f02fd6df32ff65fbe9d8
Daniel Whatley <danielwhatley@Daniels-MacBook-Pro.local>
2024-04-06 16:11:50 -0400

    Working solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,55 @@
 "use strict";
 
+const MinHeap = require('min-heap');
+
 // Print all entries, across all of the *async* sources, in chronological order.
 
 module.exports = (logSources, printer) => {
   return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+    // Use a min heap to keep log sources sorted by date of the next log line to be printed.
+    let heap = new MinHeap((l, r) => {
+      return l.nextLog.date - r.nextLog.date;
+    });
+    // First insert every source into the heap.
+    new Promise((res, rej) => {
+      let sourcesPopulated = 0;
+      for (let i = 0; i < logSources.length; i++) {
+        const src = logSources[i];
+        src.popAsync().then((log) => {
+          if (!!log) {
+            heap.insert({source: src, nextLog: log});
+          }
+          sourcesPopulated++;
+          if (sourcesPopulated == logSources.length) {
+            res();
+          }
+        });
+      }
+    }).then(() => {
+      // Then this janky setup to repeatedly look for the next log without blocking.
+      // I think this could possibly be avoided using async/await pattern rather than bare Promises.
+      (function pollForLog() {
+        setTimeout(() => {
+          if (heap.size == 0) {
+            printer.done();
+            resolve(console.log("Async sort complete."));
+          } else {
+            // Print lines until we run out of sources; when a source is drained, we don't reinsert to the heap.
+            const next = heap.removeHead();
+            const log = next.nextLog;
+            printer.print(log);
+            
+            // Get next log from the source; if one exists, reinsert source to the heap.
+            next.source.popAsync().then((nextLog) => {
+              if (!!nextLog) {
+                heap.insert({source: next.source, nextLog: nextLog})
+              }
+              pollForLog();
+            });
+          }
+        }, 0);
+      })();
+
+    });
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,34 @@
 "use strict";
 
+const MinHeap = require('min-heap');
+
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  // Use a min heap to keep log sources sorted by date of the next log line to be printed.
+  let heap = new MinHeap((l, r) => {
+    return l.nextLog.date - r.nextLog.date;
+  });
+  // First insert every source into the heap.
+  for (let src of logSources) {
+    const log = src.pop();
+    if (!!log) {
+      heap.insert({source: src, nextLog: log});
+    }
+  }
+
+  // Print lines until we run out of sources; when a source is drained, we don't reinsert to the heap.
+  while(heap.size > 0) {
+    const next = heap.removeHead();
+    const log = next.nextLog;
+    printer.print(log);
+    // Get next log from the source; if one exists, reinsert source to the heap.
+    const nextLog = next.source.pop();
+    if (!!nextLog) {
+      heap.insert({source: next.source, nextLog: nextLog})
+    }
+  }
+
+  printer.done();
   return console.log("Sync sort complete.");
 };
