5dd174cc3426a9ea9e484f3de2493bd14ca33717
Daniel Whatley <danielwhatley@Daniels-MacBook-Pro.local>
2024-04-07 12:03:00 -0400

    Add event-driven async solution and updates notes

--- solution/async-sorted-merge-original.js
+++ solution/async-sorted-merge-original.js
@@ -1,17 +1,15 @@
 "use strict";
 
-const EventEmitter = require('events');
 const MinHeap = require('min-heap');
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  // Use a min heap to keep log sources sorted by date of the next log line to be printed.
-  let heap = new MinHeap((l, r) => {
-    return l.nextLog.date - r.nextLog.date;
-  });
   return new Promise((resolve, reject) => {
-    
+    // Use a min heap to keep log sources sorted by date of the next log line to be printed.
+    let heap = new MinHeap((l, r) => {
+      return l.nextLog.date - r.nextLog.date;
+    });
     // First insert every source into the heap.
     new Promise((res, rej) => {
       let sourcesPopulated = 0;
@@ -28,28 +26,35 @@ module.exports = (logSources, printer) => {
         });
       }
     }).then(() => {
-      // Use event emitter to repeatedly fetch next log line.
-      let eventEmitter = new EventEmitter();
-      eventEmitter.on('printNextLogLine', () => {
-        if (heap.size == 0) {
+      // Then this setInterval construction to repeatedly look for the next log without blocking.
+      // I think this could possibly be avoided using async/await pattern rather than bare Promises.
+      let sourcesWithLogsRemaining = heap.size;
+      let ready = true;
+      const handle = setInterval(() => {
+        if (sourcesWithLogsRemaining == 0) {
           printer.done();
+          clearInterval(handle);
           resolve(console.log("Async sort complete."));
-        } else {
-          // Get a source and print its next log.
+        } else if (ready && heap.size > 0) {
+          // Print lines until we run out of sources; when a source is drained, we don't reinsert to the heap.
           const next = heap.removeHead();
           const log = next.nextLog;
           printer.print(log);
-          // Get another log from the source; if one exists, reinsert source to the heap.
+
+          // We have to wait for the next log from this source, to know if it is or is not immediately the next one.
+          ready = false;          
+          // Get next log from the source; if one exists, reinsert source to the heap.
           next.source.popAsync().then((nextLog) => {
             if (!!nextLog) {
               heap.insert({source: next.source, nextLog: nextLog});
+            } else {
+              sourcesWithLogsRemaining--;
             }
-            eventEmitter.emit('printNextLogLine');
+            ready = true;
           });
         }
-      });
-      // Kickstart loop with first event.
-      eventEmitter.emit('printNextLogLine');
+      }, 0);
+
     });
   });
 };
